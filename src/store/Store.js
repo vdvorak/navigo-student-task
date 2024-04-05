de8f590ec321760be086d2a00b94444d57768668
vdvorak <dvorakvitek70@gmail.com>
2024-04-05 23:09:30 +0200

    Implemented non reactive store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,47 +1,93 @@
+import { canContinueAtPath, getPropertyValueByPath } from "../utils/ObjectUtils"
+import { Validator } from "../utils/Validator"
+
 /**
- * Class for working with API
- */
-class Store {
+ * @typedef {(string | number)[]} PropertyPath
+*/
+
+export class Store {
+  constructor(data) {
     /**
-     * Method to load data using API
-     * @returns {undefined}
-     */
-    load() {
-      throw 'Not implemented!'
-    }
-  
+      * @type {object}
+      * @private
+    */
+    this._data = structuredClone(data)
+
     /**
-     * Method to get value by path.
-     * @param {(string|number)[]} path - path to data.
-     * @returns {any}
-     */
-    getValue(path) {
-      throw 'Not implemented!'
-    }
-  
-    /**
-     * Method to set value on path.
-     * @param {(string|number)[]} path - path to data.
-     */
-    setValue(path) {
-      throw 'Not implemented!'
-    }
-  
-    /**
-     * Method to check if value on path has been changed.
-     * @param {(string|number)[]} path - path to data.
-     * @returns {boolean}
-     */
-    isChanged(path) {
-      throw 'Not implemented!'
-    }
-  
-    /**
-     * Method to check if store contains any change.
-     * @returns {boolean}
-     */
-    isDirty() {
-      throw 'Not implemented!'
-    }
+      * @type {object}
+      * @private
+    */
+    this._defaultData = structuredClone(data)
+    this.inputs = new Map()
   }
-  
+
+  /**
+   * Method to get property value.
+   * @param {PropertyPath} path
+   */
+  getValue(path) {
+    return getPropertyValueByPath(path, this._data)
+  }
+
+
+  /**
+* Method to get property default value.
+* @param {PropertyPath} path
+*/
+  getDefaultValue(path) {
+    return getPropertyValueByPath(path, this._defaultData)
+  }
+
+
+  /**
+   * Method to set property value.
+   * @param {PropertyPath} path
+   * @param {any} value
+   */
+  setValue(path, value) {
+    Validator.checkPath(path)
+
+    const lastKey = path.at(-1)
+    const pathWithoutLastKey = path.slice(0, path.length - 1)
+
+    const propertyData = pathWithoutLastKey.reduce((dataObject, currentKey) => {
+      if (canContinueAtPath(dataObject, currentKey)) {
+        return dataObject[currentKey]
+      }
+
+      return dataObject
+    }, this._data)
+
+    propertyData[lastKey] = value
+  }
+
+  /**
+   * Method to get deep copy data.
+   */
+  getData() {
+    return structuredClone(this._data)
+  }
+
+  /**
+   * Method to check if value of property is not same as default value.
+   * @param {PropertyPath} path
+   * @returns {boolean}
+   */
+  isChanged(path) {
+    const value = JSON.stringify(this.getValue(path))
+    const defaultValue = JSON.stringify(this.getDefaultValue(path))
+
+    return value !== defaultValue
+  }
+
+  /**
+   * Method to check if store contains any change.
+   * @returns {boolean}
+   */
+  isDirty() {
+    const data = JSON.stringify(this._data)
+    const defaultData = JSON.stringify(this._defaultData)
+
+    return data !== defaultData
+  }
+}
