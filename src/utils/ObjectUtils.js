7b0c226730f79397deca58783f05472342763b55
vdvorak <dvorakvitek70@gmail.com>
2024-04-08 12:12:38 +0200

    Work in progress

--- src/utils/ObjectUtils.js
+++ src/utils/ObjectUtils.js
@@ -47,3 +47,16 @@ export function setPropertyValue(path, value, data) {
 export function canContinueAtPath(dataObject, propertyKey) {
     return typeof dataObject === 'object' && dataObject !== null && propertyKey in dataObject
 }
+
+/**
+ * Function to check if two values are equal.
+ * @param {any} value1
+ * * @param {any} value2
+ */
+export function isEqual(value1, value2) {
+    if(typeof dataObject === 'object'){
+        return JSON.stringify(value1) === JSON.stringify(value2)
+    } 
+
+    return value1 === value2
+}
