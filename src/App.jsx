9dd09b2bf7b62314007de49965cac7831e355f10
vdvorak <dvorakvitek70@gmail.com>
2024-04-09 21:18:30 +0200

    Editable colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,37 @@ import { users } from './store/Users'
 import { transaction } from 'mobx'
 
 const store = new MobxStore(users)
+const data = store.getData()
+const keys = Object.keys(data)
 function App() {
 
 
-  const data = store.getData()
 
-  const keys = Object.keys(data)
 
   return (
     <Observer>
       {() => <>
-        <div>
+        <div style={store.isDirty ? { transform: "translateY(30px)", transition: "0.5s ease", userSelect: "none" } : undefined}>
           <a href="https://navigo3.com/cs/" target="_blank">
             <img src={logo} className="logo" alt="Navigo3 logo" />
           </a>
         </div>
-        <div className="card">
+        {!store.isLoaded && <p>Loading...</p>}
+        {store.isLoaded && <div className="card">
           <button onClick={() => keys.forEach((key) => {
-            console.log("key", key);
-            transaction(() => store.setValue([key], `#${Math.floor(Math.random() * 16777215).toString(16)}`))
+            transaction(() => store.setValue([key], `${Math.floor(Math.random() * 16777215).toString(16)}`))
           })}>
             Přidat projeckt
           </button>
-          {keys.map((key) => <h2 key={key} style={{ padding: 5, backgroundColor: store.isChanged([key]) ? `#${Math.floor(Math.random() * 16777215).toString(16)}` : undefined }}>{key}&nbsp;{store.getValue([key])}</h2>)}
-        </div>
+          <br />
+          <br />
+          <div>
+            {keys.map((key) => {
+              const color = store.getValue([key])
+              return <div key={key} style={{ fontSize: 24, display: "flex", alignItems: "baseline", justifyContent: "center", padding: 5, verticalAlign: "middle", backgroundColor: store.isChanged([key]) ? `#${color}` : undefined }}><div>{key}&nbsp;#<input style={{ outline: "none", fontSize: 24, background: "transparent", border: "none", borderBottom: "1px solid #212529" }} type="text" value={color} onChange={e => store.setValue([key], e.target.value)} /></div></div>
+            })}
+          </div>
+        </div>}
       </>}
     </Observer>
   )
